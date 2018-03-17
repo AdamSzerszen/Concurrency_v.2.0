2b7b8e67e2a5db9525fff1e02c31e1495033c707
SOFTWAREFACTORY\owaat <adam_szerszen@interia.pl>
2018-03-17 03:56:39 +0100

    Added infrastructure elements (steering)

--- src/main.adb
+++ src/main.adb
@@ -1,6 +1,12 @@
-procedure Main is
+with Infrastructure; use Infrastructure;
 
+procedure Main is
+   test_steering: Steering;
 begin
-   --  Insert code here.
-   null;
+   test_steering.SetNumber(5);
+   test_steering.CheckStatus;
+   test_steering.Incoming(12);
+   test_steering.CheckStatus;
+   test_steering.Leaving;
+   test_steering.CheckStatus;
 end Main;
